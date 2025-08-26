43efb3c7e641732165965c141838dd3b26556ad9
Evgeniia  Chidester <janechidester1@gmail.com>
2025-08-25 22:21:50 -0400

    Changes to container carousel_part

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,75 +42,73 @@ import birthdayImg from '@/assets/images/birthday.jpg'
     </article>
   
     <article class="carousel_part">
-    
-    <h2 class="title_carousel">Our Services</h2>
-    <button class="pre_btn"><img :src="beforeImg" alt="" height="50" width="auto" /></button>
-  <button class="nxt_btn"><img :src="afterImg" alt="" height="50" width="auto" /></button>
-    
-    <section class="service_container">
-      <article class="service_card">
-        <div class="service_image"><img :src="vaccinationImg" class="service_thumb" alt="Vaccination Service" />
-        </div>
-        <div class="service_info">
-          <h3 class="service_brand">Vaccination</h3>
-          <p class="service_short_description">For extra money we vaccinate your fluffy friend against rabies.
+      <h2>Our Services</h2>
+      <button class="pre_btn"><img :src="beforeImg" alt="" height="50" width="auto" /></button>
+      <button class="nxt_btn"><img :src="afterImg" alt="" height="50" width="auto" /></button>
+      <section class="service_container">
+        <article class="service_card">
+          <div class="service_image"><img :src="vaccinationImg" class="service_thumb" alt="Vaccination Service" /></div>
+          <div class="service_info">
+            <h3 class="service_brand">Vaccination</h3>
+            <p class="service_short_description">For extra money we vaccinate your fluffy friend against rabies.
                                               We have specialists who examine your pet and give reccomendations on
                                               their weight and diet. Moreover, they examine your pet for fleas.</p>
-        </div>
-      </article>
-
-      <article class="service_card">
-        <div class="service_image"><img :src="walkingImg" class="service_thumb" alt="Walking Service" /></div>
-        <div class="service_info">
-          <h3 class="service_brand">Walking</h3>
-          <p class="service_short_description">We walk with your pet. We use cat strollers, so you should not be worried
+          </div>
+        </article>
+        
+        <article class="service_card">
+          <div class="service_image"><img :src="walkingImg" class="service_thumb" alt="Walking Service" /></div>
+          <div class="service_info">
+            <h3 class="service_brand">Walking</h3>
+            <p class="service_short_description">We walk with your pet. We use cat strollers, so you should not be worried
                                               that our friend will run away. If your friend stays at the hotel in winter,
                                               we provide warm coat, hat and socks and boots.</p>
-        </div>
-      </article>
+          </div>
+        </article>
       
-      <article class="service_card">
-        <div class="service_image"><img :src="menuImg" class="service_thumb" alt="Menu" /></div>        
-        <div class="service_info">
-          <h3 class="service_brand">Cats' Menu</h3>
-          <p class="service_short_description">We have a menu for our guests. If your pet has a special diet or has preferences, 
+        <article class="service_card">
+          <div class="service_image"><img :src="menuImg" class="service_thumb" alt="Menu" /></div>        
+          <div class="service_info">
+            <h3 class="service_brand">Cats' Menu</h3>
+            <p class="service_short_description">We have a menu for our guests. If your pet has a special diet or has preferences, 
                                               you can examine the menu and let our staff know which your pet would like to eat 
                                               during their staying at the hotel.</p>
-        </div>
-      </article>
-
-      <article class="service_card">
-        <div class="service_image"><img :src="showerImg" class="service_thumb" alt="Shower" /></div>        
-        <div class="service_info">
-          <h3 class="service_brand">Cutting Nails and Hair</h3>
-          <p class="service_short_description">For extra money we can wash and cut your pet's hair if you are interested in 
+          </div>
+        </article>
+        
+        <article class="service_card">
+          <div class="service_image"><img :src="showerImg" class="service_thumb" alt="Shower" /></div>
+          <div class="service_info">
+            <h3 class="service_brand">Cutting Nails and Hair</h3>
+            <p class="service_short_description">For extra money we can wash and cut your pet's hair if you are interested in 
                                               this service. Moreover, we cut your pet's nails. We use only shampoos, which don't irritate 
                                               their skin.</p>
-        </div>
-      </article>
-      
-      <article class="service_card">
-        <div class="service_image"><img :src="catShopImg" class="service_thumb" alt="Cat Shop" /></div>        
-        <div class="service_info">
-          <h3 class="service_brand">Cats' Toys</h3>
-          <p class="service_short_description">If your pet likes toys and other accessories in our hotel, 
+          </div>
+        </article>
+        
+        <article class="service_card">
+          <div class="service_image"><img :src="catShopImg" class="service_thumb" alt="Cat Shop" /></div>
+          <div class="service_info">
+            <h3 class="service_brand">Cats' Toys</h3>
+            <p class="service_short_description">If your pet likes toys and other accessories in our hotel, 
                                               you will be able to buy them at the cats' store, which is located inside the hotel. 
                                               The prices there are lower than in other pets' shops.</p>
-        </div>
+          </div>
       
-      </article>
-      
-      <article class="service_card">
-        <div class="service_image"><img :src="birthdayImg" class="service_thumb" alt="Birthday Party" /></div>       
-        <div class="service_info">
-          <h3 class="service_brand">Birthday Party</h3>
-          <p class="service_short_description"> We organise birthday parties for our guests. If your friend has a birthday
+        </article>
+        
+        <article class="service_card">
+          <div class="service_image"><img :src="birthdayImg" class="service_thumb" alt="Birthday Party" /></div>
+          <div class="service_info">
+            <h3 class="service_brand">Birthday Party</h3>
+            <p class="service_short_description"> We organise birthday parties for our guests. If your friend has a birthday
                                                 while they are staying at our hotel, we will be happy to make this day 
                                                 memorable for your pet.</p>
-        </div>
-      </article>
-    </section>
-  
+          </div>
+        </article>
+
+      </section>
+    
     </article>
 
 </section>
