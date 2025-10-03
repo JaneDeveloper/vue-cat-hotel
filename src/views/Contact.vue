478cc2472714c44c35716b1243cc26cf270099bf
Evgeniia  Chidester <janechidester1@gmail.com>
2025-10-02 22:59:13 -0400

    some changes to form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -56,18 +56,19 @@
             <div class="date-picker">
                 <label for="checkin-month">Month</label>
                 <select id="checkin-month" name="checkin-month">
-                    <option value="01">January</option>
-                    <option value="02">February</option>
-                    <option value="03">March</option>
-                    <option value="04">April</option>
-                    <option value="05">May</option>
-                    <option value="06">June</option>
-                    <option value="07">July</option>
-                    <option value="08">August</option>
-                    <option value="09">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
+                    <option value="01" data-short="Jan">January</option>
+<option value="02" data-short="Feb">February</option>
+<option value="03" data-short="Mar">March</option>
+<option value="04" data-short="Apr">April</option>
+<option value="05" data-short="May">May</option>
+<option value="06" data-short="Jun">June</option>
+<option value="07" data-short="Jul">July</option>
+<option value="08" data-short="Aug">August</option>
+<option value="09" data-short="Sep">September</option>
+<option value="10" data-short="Oct">October</option>
+<option value="11" data-short="Nov">November</option>
+<option value="12" data-short="Dec">December</option>
+
                 </select>
                 
                 
